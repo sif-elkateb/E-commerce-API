e5c00d839ad8b4d2fa216ada7b2c3ad4a81c85b7
seif <sifelkateb@gmail.com>
2022-07-24 01:59:09 +0200

    setup cookies

--- controller/authController.js
+++ controller/authController.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const UserModel = require("../models/user");
 const { createJWT } = require("../utils");
+const date = require('date-and-time');
 
 
 const registerUser=async(req,res,next)=>{
@@ -15,11 +16,14 @@ const registerUser=async(req,res,next)=>{
     const role=(await UserModel.countDocuments({}))===0?'admin':'user'
     const user=await UserModel.create({name,email,password,role});
 
-    const tokenUser={name:user.name,userId:user._id};
+    const tokenUser={name:user.name,userId:user._id,role};
 
     const token=createJWT({payload:tokenUser});
+    const now = new Date();
+    const cookieExpireDate = date.addDays(now,30);
+    res.cookie('token',token,{httpOnly:true,expires:cookieExpireDate})
 
-    res.status(StatusCodes.CREATED).json({user,token})
+    res.status(StatusCodes.CREATED).json({user:tokenUser})
 }
 const loginUser=async(req,res,next)=>{
     res.status(StatusCodes.OK).json({msg:'user loggedin successfully'})
